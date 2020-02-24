888d16320291c8da9674fe1a092baf2662becbfc
Sandy <sandy.jernigan@gmail.com>
2020-02-24 00:18:20 -0500

    storage is a list that holds the Item's that are currently in the room

--- src/room.py
+++ src/room.py
@@ -2,10 +2,28 @@
 # description attributes.
 
 class Room:
-  def __init__(self, name, shortname, description):
+  def __init__(self, name, description):
+    # The room should have `name` and `description` attributes.
     self.name = name
     self.shortname = shortname
     self.description = description
 
+    # The room should also have `n_to`, `s_to`, `e_to`, and `w_to` attributes
+    # which point to the room in that respective direction.
+    self.n_to: None
+    self.e_to: None
+    self.s_to: None
+    self.w_to: None
+
+    # storage is a list that holds the Item's that are currently in the room
+    self.storage = []
+
   def __str__(self):
     return f"{self.shortname}"
+  
+  def listStorage(self):
+    i = 1
+    for e in self.storage:
+      print(f"{i} - {e}")
+      i = i + 1
+  
