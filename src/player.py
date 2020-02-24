90b9194fd8857ba2df89828bc552d14211bb1aa2
Sandy <sandy.jernigan@gmail.com>
2020-02-24 01:20:49 -0500

    Update player.py

--- src/player.py
+++ src/player.py
@@ -5,3 +5,12 @@ class Player:
   def __init__(self, name, room):
     self.name = name
     self.room = room
+
+    # inventory is a list that holds the Item's that are currently in the player's inventory
+    self.inventory = []
+  
+  def listInventory(self):
+    i = 1
+    for e in self.inventory:
+      print(f"{i} - {e}")
+      i = i + 1
